6b9fea5bf0fba50637cbab03ccc264e951c35efc
Serhii <serhii.samoliuk@outlook.com>
2023-07-22 18:42:18 +0200

    add custom background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { Canvas } from '@react-three/fiber';
+import { Experience } from './components/Experience';
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
       <Experience />
     </Canvas>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,17 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from '@react-three/drei';
+import * as THREE from 'three';
 
 export const Experience = () => {
+  const map = useTexture('textures/water-world.jpg');
+
   return (
     <>
+      <ambientLight intensity={0.5} />
+      <Environment preset={'dawn'} />
       <OrbitControls />
       <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
+        <sphereGeometry args={[5, 64, 64]} />
+        <meshStandardMaterial map={map} side={THREE.BackSide} />
       </mesh>
     </>
   );
